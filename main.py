016934042c23a826393cf0cdd2d9512ad6d98b6d
payalpagar <151488423+payalpagar@users.noreply.github.com>
2025-03-17 17:15:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello,Everyone World!"}
+    return {"message": "Hello, World!"}
 
 @app.get("/goodmorning/{name}")
 def say_good_morning(name: str):
-    return {"message": f"very Good morning, {name}!"}
+    return {"message": f"very Good morning, {Payal}!"}
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
